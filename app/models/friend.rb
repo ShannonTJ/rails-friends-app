269ed50a2accc79544c67c8ede1fdff44439248e
Shannon TJ <shannontj@shaw.ca>
2021-03-13 18:59:50 -0700

    default values for email phone and twitter

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,18 @@
 class Friend < ApplicationRecord
     belongs_to :user
+
+    before_save :set_email, :set_phone, :set_twitter
+
+    def set_email
+        self.email = "(no email)" if self.email.blank?
+    end
+
+    def set_phone
+        self.phone = "(no phone #)" if self.phone.blank?
+    end
+
+    def set_twitter
+        self.twitter = "(no twitter)" if self.twitter.blank?
+    end
+
 end
